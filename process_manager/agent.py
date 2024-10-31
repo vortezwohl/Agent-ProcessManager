9727041ae51aba46d7ce1367e095baaef34ec9d0
zihaowu <zihao.wu@igg.com>
2024-10-31 22:42:22 +0800

    init

--- process_manager/agent.py
+++ process_manager/agent.py
@@ -1,9 +1,7 @@
-import os
-
+from dotenv import load_dotenv
 from ceo import Agent, get_openai_model
 
 from process_manager.action import get_process_by_name, show_all_processes
 
-os.environ['OPENAI_API_KEY'] = 'sk-mOzqTNBFENiFIilFT-gNaUaUR7GbbjXSj7oGychzSqT3BlbkFJWQjUtSGrwkfU3nd5m-GAN_pleB8VaRWFDrFMTHol4A'
-
+load_dotenv()
 process_manager = Agent([get_process_by_name, show_all_processes], get_openai_model())
